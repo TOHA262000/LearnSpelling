74f8495f0352b04dbab9e1a5c7e419d9c938196b
Md. Toha <m.i.toaha14@gmail.com>
2025-05-30 10:05:42 +0600

    Footer section add

--- src/App.js
+++ src/App.js
@@ -4,17 +4,22 @@ import PracticeMode from './pages/PracticeMode/PracticeMode';
 import WordManagement from './pages/WordManagement/WordManagement';
 import SpellingTest from './pages/SpellingTest/SpellingTest';
 import NaVBar from './pages/NavBar/NavBar';
+import Footer from './pages/Footer/Footer';
 
 export default function App() {
   return (
     <Router>
-      <div className="bg-gray-900 min-h-screen  text-black">
+      <div className="bg-gray-900 min-h-screen flex flex-col text-black">
         <NaVBar />
-        <Routes>
-          <Route path="/" element={<PracticeMode />} />
-          <Route path="/words" element={<WordManagement />} />
-          <Route path="/test" element={<SpellingTest />} />
-        </Routes>
+        {/* This div will take remaining space */}
+        <div className="flex-grow">
+          <Routes>
+            <Route path="/" element={<PracticeMode />} />
+            <Route path="/words" element={<WordManagement />} />
+            <Route path="/test" element={<SpellingTest />} />
+          </Routes>
+        </div>
+        <Footer />
       </div>
     </Router>
   );
---
+++ src/pages/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <footer className="bg-gray-800 text-white text-center py-4">
+            <p>© 2025 SpellMaster. Developed by Toha.</p>
+            <p>
+                GitHub: <a href="https://github.com/TOHA262000" className="underline hover:text-yellow-300" target="_blank" rel="noopener noreferrer">TOHA262000</a>
+            </p>
+        </footer>
+    )
+}
