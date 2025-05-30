5cf53c639183a9a0f13993f45e6c6005dd974240
Md. Toha <m.i.toaha14@gmail.com>
2025-05-30 10:47:08 +0600

    Solve Footer issue in mobile device

--- src/pages/Footer/Footer.js
+++ src/pages/Footer/Footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
         <footer className="bg-gray-800 text-white text-center py-4">
             <p>© 2025 SpellMaster. Developed by Toha.</p>
             <p>
-                GitHub: <a href="https://github.com/TOHA262000" className="underline hover:text-yellow-300" target="_blank" rel="noopener noreferrer">TOHA262000</a>
+                GitHub: <a href="https://github.com/TOHA262000" className="underline hover:text-yellow-300" target="_blank" rel="noopener noreferrer">TOHA</a>
             </p>
         </footer>
     )
